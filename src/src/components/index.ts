a34a398e461baa0d5c05caf9ab445433fa9aa4d6
SittisukC1 <sittisukc@cariva.co.th>
2025-02-28 17:31:10 +0700

    change name

--- src/src/components/index.ts
+++ src/src/components/index.ts
@@ -1,3 +1,3 @@
-export { default as Navbar } from './Navbar'
+export { default as Navbar } from './navbars'
 export { default as TitleSection } from './titleSection'
 export { default as HighLightText } from './highLightText'
--- src/src/screen/index.ts
+++ src/src/screen/index.ts
@@ -1,4 +1,4 @@
-export { default as HomeScreen } from './Home'
+export { default as HomeScreen } from './homes'
 export { default as SkillScreen } from './skills'
 export { default as PortfolioScreen } from './portfolio'
 export { default as ContantScreen } from './contact'
